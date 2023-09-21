f5c98dcb1871b1a696a8abc1ca90a047aaac9a85
saifullah0317 <79357538+saifullah0317@users.noreply.github.com>
2023-09-21 08:49:37 +0500

    tf-tdf-scoring-addes

--- api/index.py
+++ api/index.py
@@ -1,44 +1,19 @@
-from flask import Flask,request, jsonify
+from flask import Flask, request, jsonify
 import os
 import re
 import PyPDF2
-# import docx
 from docx import Document
 from bs4 import BeautifulSoup
 import io
 import requests
+import math
+from collections import Counter
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    filenames=[]
-    document_index = extractText(filenames)
-    queries = request.args.getlist('query')
-    results=search(queries,document_index)
-    try:
-        resp=[]
-        for filename,count in results.items():
-            resp.append({
-                "filename":filename,
-                "termFrequency":count
-            })
-        for filename in filenames:
-            for query in queries:
-                if query in filename:
-                    resp.append({
-                        "filename":filename
-                    })
-        if resp:
-            return jsonify(resp)
-        else:
-            return jsonify({"message":"no reuslt matched !"})
-    except Exception as err:
-        return jsonify({"error":err})
-
-# Step 2: Gather Documents, text extraction and indexing
-def extractText(filenames):
-    index={}
+# Step 2: Gather Your Documents
+def gather_documents():
+    documents = []
     repo = 'saifullah0317/search-engine-documents'
     folderpath = 'documents/'  # Remove 'tree/main' from the path
 
@@ -64,7 +39,7 @@ def extractText(filenames):
                 if file_response.status_code == 200:
                     file_content = file_response.content
                     file_name = os.path.basename(file_path)
-                    filenames.append(file_name)
+
                     # Check file extension to determine how to process
                     if file_name.endswith('.pdf'):
                         # PDF file
@@ -90,31 +65,74 @@ def extractText(filenames):
                         text = file_content.decode('utf-8')
 
                     else:
-                        continue
-                    words=re.findall(r"\w+", text.lower())
-                    for word in words:
-                        if word not in index:
-                            index[word]=[]
-                        index[word].append(file_name)
+                        continue  # Unsupported file type
+
+                    documents.append({
+                        'filename': file_name,
+                        'content': text
+                    })
                 else:
                     print(f'Failed to retrieve file content from GitHub: {file_path}')
-                    return
-        return index
     else:
         print('Failed to list folder contents from GitHub.')
-        return
 
-# Step 6: Build a Search Function
-def search(queries, index):
-    # query_words = re.findall(r"\w+", query.lower())
-    matching_documents = {}
-    for word in queries:
-        if word in index:
-            for filename in index[word]:
-                if filename not in matching_documents:
-                    matching_documents[filename] = 0
-                matching_documents[filename] += 1
-    return matching_documents
+    return documents
 
-if __name__=="__main__":
-    app.run(debug=True,port=3000)
+documents = gather_documents()
+
+# Calculate IDF (Inverse Document Frequency)
+def calculate_idf(documents, keyword):
+    total_documents = len(documents)
+    documents_containing_keyword = sum(1 for doc in documents if keyword in doc['content'])
+    
+    if documents_containing_keyword == 0:
+        return 0.0  # Avoid division by zero
+    else:
+        return math.log10(total_documents / documents_containing_keyword)
+
+# Calculate TF (Term Frequency)
+def calculate_tf(document, keyword):
+    word_list = re.findall(r'\w+', document.lower())
+    word_count = Counter(word_list)
+    return word_count[keyword] / len(word_list)
+
+# Step 3: Create a Query Function
+def search(query):
+    # Step 4: Implement Keyword Matching with TF-IDF Scoring
+    results = []
+    query_keywords = re.findall(r'\w+', query.lower())
+    
+    for document in documents:
+        document_keywords = re.findall(r'\w+', document['content'].lower())
+        matched_keywords = set(query_keywords).intersection(document_keywords)
+        
+        # Calculate TF-IDF score for the document
+        tfidf_score = sum(calculate_tf(document['content'], keyword) * calculate_idf(documents, keyword) for keyword in matched_keywords)
+        
+        if tfidf_score > 0:  # Only include documents with a non-zero TF-IDF score
+            results.append({
+                'filename': document['filename'],
+                'tfidf_score': tfidf_score
+            })
+    
+    # Step 6: Display the Ranked Documents
+    results.sort(key=lambda x: x['tfidf_score'], reverse=True)
+    return results
+
+
+@app.route('/')
+def home():
+    query = request.args.get('query', '')
+    
+    if not query:
+        return jsonify({'message': 'Please provide a query.'})
+    # print(documents)
+    results = search(query)
+    
+    if results:
+        return jsonify(results)
+    else:
+        return jsonify({'message': 'No results matched your query.'})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
